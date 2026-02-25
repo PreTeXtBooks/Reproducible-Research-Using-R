b864eee45208c0f48460923839b2e5652113e480
martinezc1 <c.martinez0@outlook.com>
2026-02-25 17:25:07 -0500

    Added package citations to each of the chapters

---
+++ packages-bib-creation.R
@@ -0,0 +1,46 @@
+pkgs <- c(
+  "base",
+  "tidyverse",
+  "palmerpenguins",
+  "fortunes",
+  "cowsay",
+  "nycOpenData",
+  "quartets",
+  "knitr",
+  "ggplot2",
+  "mosaic",
+  "supernova",
+  "AICcmodavg",
+  "readxl",
+  "skimr",
+  "janitor",
+  "ggthemes",
+  "patchwork",
+  "webshot2",
+  "plotly",
+  "gmodels",
+  "pheatmap",
+  "rcompanion",
+  "kableExtra",
+  "GGally",
+  "ppcor",
+  "ggformula",
+  "lmtest",
+  "caTools",
+  "ISLR2",
+  "pscl",
+  "caret",
+  "car",
+  "pROC",
+  "broom",
+  "stats",
+  "magrittr",
+  "tibble",
+  "dplyr"
+)
+
+bib_entries <- unlist(lapply(pkgs, function(p) {
+  toBibtex(citation(p))
+}))
+
+writeLines(bib_entries, "packages.bib")
