85d0ea965392bef7f254c1f9c971ebd4a3a861d7
martinezc1 <c.martinez0@outlook.com>
2026-02-25 21:13:03 -0500

    Fixed .bib

--- packages-bib-creation.R
+++ packages-bib-creation.R
@@ -1,45 +1,10 @@
 pkgs <- c(
-  "base",
-  "tidyverse",
-  "palmerpenguins",
-  "fortunes",
-  "cowsay",
-  "nycOpenData",
-  "quartets",
-  "knitr",
-  "ggplot2",
-  "mosaic",
-  "supernova",
-  "readxl",
-  "skimr",
-  "janitor",
-  "ggthemes",
-  "patchwork",
-  "webshot2",
-  "plotly",
-  "gmodels",
-  "pheatmap",
-  "rcompanion",
-  "kableExtra",
-  "GGally",
-  "ppcor",
-  "ggformula",
-  "lmtest",
-  "caTools",
-  "ISLR2",
-  "pscl",
-  "caret",
-  "car",
-  "pROC",
-  "broom",
-  "stats",
-  "magrittr",
-  "tibble",
-  "dplyr"
+  "base","tidyverse","palmerpenguins","fortunes","cowsay","nycOpenData","quartets",
+  "knitr","ggplot2","mosaic","supernova","readxl","skimr","janitor",
+  "ggthemes","patchwork","webshot2","plotly","gmodels","pheatmap","rcompanion",
+  "kableExtra","GGally","ppcor","ggformula","lmtest","caTools","ISLR2",
+  "pscl","caret","car","pROC","broom","stats","magrittr","tibble","dplyr"
 )
 
-bib_entries <- unlist(lapply(pkgs, function(p) {
-  toBibtex(citation(p))
-}))
-
-writeLines(bib_entries, "packages.bib")
+# This writes a valid BibTeX file with proper keys
+knitr::write_bib(pkgs, file = "packages.bib")
